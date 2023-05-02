4d389a32127b9145df9568a3eb9e75ab92be5426
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-05-02 16:55:30 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,70 +1,61 @@
 import s from './ContactForm.module.css';
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export default class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export default function ContactForm({ addContact }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  onInputChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+  const onInputChange = e => {
+    const input = e.target;
+    switch (input.name) {
+      case 'name':
+        setName(input.value);
+        break;
+      case 'number':
+        setNumber(input.value);
+        break;
+      default:
+        return;
+    }
   };
-  // onFormSubmit = e => {
-  //   e.preventDefault();
-  //   // console.log(this.state.name);
-  //   this.props.isNamesDublicated(this.state.name)
-  //     ? alert(`${this.state.name} is already in contacts.`)
-  //     : this.props.addContact({ ...this.state, id: nanoid() });
-
-  //   this.setState({
-  //     name: '',
-  //     number: '',
-  //   });
-  // };
-  onFormSubmit = e => {
+  const onFormSubmit = e => {
     e.preventDefault();
-    this.props.addContact(this.state);
-    this.setState({
-      name: '',
-      number: '',
-    });
+    addContact({ name: name, number: number });
+    setName('');
+    setNumber('');
   };
-
-  render() {
-    return (
-      <form className={s.wrapper} action="" onSubmit={this.onFormSubmit}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          value={this.state.name}
-          onChange={this.onInputChange}
-        />
-        <label htmlFor="number">Number</label>
-        <input
-          id="number"
-          type="text"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          value={this.state.number}
-          onChange={this.onInputChange}
-        />
-        <button className={s.button} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form className={s.wrapper} action="" onSubmit={onFormSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        id="name"
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={name}
+        onChange={onInputChange}
+      />
+      <label htmlFor="number">Number</label>
+      <input
+        id="number"
+        type="text"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={number}
+        onChange={onInputChange}
+      />
+      <button className={s.button} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
+
 ContactForm.propTypes = {
   addContact: PropTypes.func,
 };
